fbba6fdd1bc2d40af3a21fa9dc0fb86d338086df
arjun-krishnan <91309634+arjun-krishnan@users.noreply.github.com>
2026-04-24 19:42:49 +0000

    style: auto-format code with Black

--- src/laser_modulator/tracking.py
+++ src/laser_modulator/tracking.py
@@ -5,30 +5,40 @@ Electron Tracking Module
 This module contain the functions for tracking the ensemble of electrons
 through the given magnetic lattice (laser modulator or a chicane)
 """
+
 import numpy as np
 import sys
 from time import time
 import scipy.constants as const
 from scipy import special
 
-
 ##### natural constants #####
-c = const.c                     # speed of light
-e_charge = const.e              # electron charge
-m_e = const.m_e                 # electron mass in eV/c^2
-Z0 = 376.73                     # impedance of free space in Ohm
-epsilon_0 = const.epsilon_0     # vacuum permittivity
-mu0 = const.mu_0                # vacuum permeability
+c = const.c  # speed of light
+e_charge = const.e  # electron charge
+m_e = const.m_e  # electron mass in eV/c^2
+Z0 = 376.73  # impedance of free space in Ohm
+epsilon_0 = const.epsilon_0  # vacuum permittivity
+mu0 = const.mu_0  # vacuum permeability
 
-def lsrmod_track(Mod, Lsr, e_bunch, Lsr2=None, tstep=1e-12, zlim=None, plot_track=False, disp_Progress=True):
+
+def lsrmod_track(
+    Mod,
+    Lsr,
+    e_bunch,
+    Lsr2=None,
+    tstep=1e-12,
+    zlim=None,
+    plot_track=False,
+    disp_Progress=True,
+):
     N_e = len(e_bunch[0])
     bunch = np.copy(e_bunch)
     z_0 = np.mean(bunch[2])
     bunch[2] -= z_0
-    z_mean = np.mean(bunch[2]) 
-    
+    z_mean = np.mean(bunch[2])
+
     progressrate = 10
-    progress = 0 
+    progress = 0
     t = 0
 
     starttime = time()
@@ -37,67 +47,79 @@ def lsrmod_track(Mod, Lsr, e_bunch, Lsr2=None, tstep=1e-12, zlim=None, plot_trac
     track_z = []
     dZZ = []
     ZZ = []
-    
+
     if zlim == None:
         zlim = Mod.len
-    
+
     while z_mean < z_0 + zlim:
         if disp_Progress:
             if progress < (z_mean) / zlim * progressrate:
                 elapsed = time() - starttime
-                sys.stdout.write('\r Progress: ' + str(progress) + '/' + str(progressrate) + " \t ETA: " + 
-                                 str(np.round(elapsed/60 * (progressrate / (progress+0.01) - 1), 2)) + " mins ")
+                sys.stdout.write(
+                    "\r Progress: "
+                    + str(progress)
+                    + "/"
+                    + str(progressrate)
+                    + " \t ETA: "
+                    + str(
+                        np.round(
+                            elapsed / 60 * (progressrate / (progress + 0.01) - 1), 2
+                        )
+                    )
+                    + " mins "
+                )
                 sys.stdout.flush()
                 progress += 1
-    
+
         z = np.copy(bunch[2])
         z_mean = np.mean(z)
         ZZ.append(z_mean)
-        
-        Efield_x_vec = Lsr.E_field(bunch[0],bunch[1],bunch[2],t)
-        if Lsr2 != None: 
-            Efield_x_vec += Lsr2.E_field(bunch[0],bunch[1],bunch[2],t)
+
+        Efield_x_vec = Lsr.E_field(bunch[0], bunch[1], bunch[2], t)
+        if Lsr2 != None:
+            Efield_x_vec += Lsr2.E_field(bunch[0], bunch[1], bunch[2], t)
         EE.append(Efield_x_vec[0])
 
         try:
             Bfield_y_vec = Mod.B_func(z) + Efield_x_vec / c
         except:
             Bfield_y_vec = Efield_x_vec / c
-        
+
         p_field = bunch[3:]
         p_vec = np.sqrt(np.sum(p_field**2, axis=0))
         gamma_vec = np.sqrt((p_vec / m_e / c) ** 2 + 1)
-        dp_x_vec = (Efield_x_vec - p_field[2] * Bfield_y_vec / m_e / gamma_vec) * e_charge * tstep
+        dp_x_vec = (
+            (Efield_x_vec - p_field[2] * Bfield_y_vec / m_e / gamma_vec)
+            * e_charge
+            * tstep
+        )
         dp_y_vec = np.zeros(N_e)
-        dp_z_vec = p_field[0] * Bfield_y_vec / m_e / gamma_vec * e_charge * tstep   
-        p_new = bunch[3:] + [dp_x_vec , dp_y_vec , dp_z_vec]
-        p_vec_new = np.sqrt(np.sum(p_new**2 , axis=0))
-        gamma_vec_new = np.sqrt((p_vec_new / m_e / c)**2 + 1)    
-                       
-        spatial_new = bunch[0:3,:] + p_new / m_e / gamma_vec_new * tstep       
+        dp_z_vec = p_field[0] * Bfield_y_vec / m_e / gamma_vec * e_charge * tstep
+        p_new = bunch[3:] + [dp_x_vec, dp_y_vec, dp_z_vec]
+        p_vec_new = np.sqrt(np.sum(p_new**2, axis=0))
+        gamma_vec_new = np.sqrt((p_vec_new / m_e / c) ** 2 + 1)
+
+        spatial_new = bunch[0:3, :] + p_new / m_e / gamma_vec_new * tstep
         bunch[0:3] = np.copy(spatial_new)
         bunch[3:] = np.copy(p_new)
-        
+
         t += tstep
-        
+
         track_x.append(np.copy(bunch[0][-6:]))
         track_z.append(np.copy(bunch[2][-6:]))
-        
-        dz = (t * c - np.mean(bunch[2]))
+
+        dz = t * c - np.mean(bunch[2])
         dZZ.append(dz)
-  
 
     if disp_Progress:
-            print('Progress: '+str(progress)+'/'+str(progressrate))
+        print("Progress: " + str(progress) + "/" + str(progressrate))
 
     if plot_track == True:
         track_x = np.array(track_x)
         track_z = np.array(track_z)
-        return(bunch,track_x,track_z)
-    
-    endtime = time()
-    print("\nRuntime:  " , np.round(endtime-starttime,2) , " sec")
-    
-    return bunch  
-        
+        return (bunch, track_x, track_z)
 
+    endtime = time()
+    print("\nRuntime:  ", np.round(endtime - starttime, 2), " sec")
+
+    return bunch
--- tests/test_tracking.py
+++ tests/test_tracking.py
@@ -9,27 +9,30 @@ import numpy as np
 from laser_modulator.components import Modulator, Laser
 from laser_modulator.tracking import lsrmod_track
 
+
 def test_tracking_zero_laser_power(tmp_path):
     mod_file = tmp_path / "mod_params.dat"
     with open(mod_file, "w") as f:
         f.write("START\nE0: 1492\nWL: 800e-9\nNPERIOD: 6\nPERIODLEN: 0.15\nEND\n")
     my_mod = Modulator(str(mod_file), plot=False)
-    
+
     # Create a dummy Laser with EXACTLY ZERO pulse energy
     laser_file = tmp_path / "laser_params.dat"
     with open(laser_file, "w") as f:
         f.write("START\nWL: 800e-9\nE: 0.0\nEND\n")  # Pulse energy E is 0!
     my_laser = Laser(str(laser_file))
-    
+
     N_e = 100
     e_bunch = np.zeros((6, N_e))
     initial_energy_spread = np.random.normal(0, 7e-4, N_e)
-    e_bunch[5,:] = initial_energy_spread
-    
-    energy_start = np.copy(e_bunch[5,:])
-    
-    e_bunch = lsrmod_track(my_mod, my_laser, e_bunch, plot_track=False, disp_Progress=False)
-    
-    energy_end = e_bunch[5,:]
-    
+    e_bunch[5, :] = initial_energy_spread
+
+    energy_start = np.copy(e_bunch[5, :])
+
+    e_bunch = lsrmod_track(
+        my_mod, my_laser, e_bunch, plot_track=False, disp_Progress=False
+    )
+
+    energy_end = e_bunch[5, :]
+
     np.testing.assert_allclose(energy_start, energy_end, atol=1e-10)
